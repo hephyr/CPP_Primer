0955cb4ac3620cbc4f1f5fe68f87ebf0c5318747
Zephyr <ixuhefeng@gmail.com>
2015-10-14 19:51:47 +0800

    add

--- 3/3.5.3/3_36.cc
+++ 3/3.5.3/3_36.cc
@@ -2,16 +2,54 @@
 #include <vector>
 using namespace std;
 int main() {
-    point
-    // int arr1[5], arr2[5];
-    // while ()
-    // for (int i = 0; i < sizeof(arr1) / sizeof(int); i++)
-    //     if (arr1[i] != arr2[i]) {
-    //         cout << "Two array is different !" << endl;
-    //         break;
-    //     }
-    // if (i == sizeof(arr1) / sizeof(int))
-    //     cout << "Two array is identical ! " << endl;
-    vector<int> v1, v2;
+    const int arr_size = 5;
+    int arr1[arr_size], arr2[arr_size];
+    int *p1 = arr1;
+    int *p2 = arr2;
+    int i;
+    cout << "Input array1 element" << endl;
+    while (p1 != end(arr1) && cin >> i) {
+        *p1 = i;
+        p1++;
+    }
+    cout << "Input array2 element" << endl;
+    while (p2 != end(arr2) && cin >> i) {
+        *p2 = i;
+        p2++;
+    }
+    p1 = arr1;
+    p2 = arr2;
+    while (p1 != end(arr1)) {
+        if (*p1 == *p2) {
+            p1++;
+            p2++;
+            continue;
+        }
+        else {
+            cout << "Two array are different." << endl;
+            break;
+        }
+    }
+    if (p1 == end(arr1))
+        cout << "Two array are identical." << endl;
+
+    //vector
+    vector<int> v1(arr_size), v2(arr_size);
+    auto it = v1.begin();
+    cout << "Input v1 element" << endl;
+    while (it != v1.end() && cin >> i) {
+        *it = i;
+        it++;
+    }
+    it = v2.begin();
+    cout << "Input v2 element" << endl;
+    while (it != v2.end() && cin >> i) {
+        *it = i;
+        it++;
+    }
+    if (v1 == v2)
+        cout << "Two vector are identical." << endl;
+    else
+        cout << "Two vector are different." << endl;
     return 0;
 }
