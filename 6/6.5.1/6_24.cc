79f5c9bb9f8e8ab9bbb9fc63cbaa42fad04ca028
Zephyr <ixuhefeng@gmail.com>
2015-10-23 22:01:52 +0800

    add

---
+++ 6/6.5.1/6_24.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+string make_plural(size_t ctr, const string &word, const string &ending = "s");
+int main() {
+    cout << "singual: " << make_plural(1, "success", "es") << " "
+         << make_plural(1, "failure") << endl;
+    cout << "plural : " << make_plural(2, "success", "es") << " "
+         << make_plural(2, "failure") << endl;
+    return 0;
+}
+string make_plural(size_t ctr, const string &word, const string &ending) {
+    return (ctr > 1) ? word + ending : word;
+}
