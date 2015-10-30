535aa1e5109ce969a15d81d1ae3ba84ca42290a0
Zephyr <ixuhefeng@gmail.com>
2015-10-30 21:58:33 +0800

    7_52

---
+++ 7/7.5.5/7_52.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+struct Sales_data {
+    std::string bookNo;
+    unsigned units_sold;
+    double revenue;
+};
+int main() {
+    Sales_data item = {"978-0590353403", 25, 15.99};
+    return 0;
+}
