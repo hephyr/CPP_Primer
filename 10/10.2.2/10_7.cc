e01de49c2ec64f8c6fede1730e9f6e167e5c09c9
Zephyr <ixuhefeng@gmail.com>
2015-11-17 14:28:49 +0800

    10.2.2.

--- 10/10.2.2/10_7.cc
+++ 10/10.2.2/10_7.cc
@@ -14,5 +14,8 @@ int main() {
 		lst.push_back(i);
 	vec.resize(lst.size());
 	std::copy(lst.cbegin(), lst.cend(), vec.begin());
+	vector<int> v;
+	v.reserve(10);
+	std::fill_n(v.begin(), 10, 0);
 	return 0;
 }
