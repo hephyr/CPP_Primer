4b2fbdb822b1e8447406e60935d7e4b6a1a63633
Zephyr <ixuhefeng@gmail.com>
2015-11-23 23:40:14 +0800

    10.3.2

---
+++ 10/10.3.2/10_14.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <algorithm>
+using std::cout; using std::endl;
+
+
+int main() {
+	auto f = [](int a, int b) { return a + b; };
+	cout << f(1, 2) << endl;
+	return 0;
+}
