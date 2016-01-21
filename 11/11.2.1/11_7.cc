103190eb34b92003e71c6fabeef00750bbe73b02
Zephyr <ixuhefeng@gmail.com>
2016-01-21 20:41:56 +0800

    recode 11_7.cc

--- 11/11.2.1/11_7.cc
+++ 11/11.2.1/11_7.cc
@@ -12,27 +12,18 @@ int main() {
     int i;
     string str, name;
     while (1) {
-        cout << "1.add family" << endl;
-        cout << "2.add child" << endl;
-        cout << "3.traversal" << endl;
-        cout << "4.exit" << endl;
+        cout << "1.add child" << endl;
+        cout << "2.traversal" << endl;
+        cout << "3.exit" << endl;
         cin >> i;   
         switch(i) {
-            case 1: cout << "Input the family name :";
-                    cin >> str;
-                    people.insert(std::pair<string, vector<string>> (str, vector<string>{}));
-                    break;
-            case 2: cout << "FAMILY" << endl;
-                   for (const auto &family : people) {
-                       cout << family.first << endl;
-                   }
-                   cout << "which family ?" << endl;
-                   cin >> str;
+            case 1:cout << "which family ?" << endl;
                    cout << "Input the child name" << endl;
                    cin >> name;
+                   cin >> str;
                    people[str].push_back(name);
                    break;
-            case 3: for (const auto &family : people) {
+            case 2: for (const auto &family : people) {
                         cout << family.first << endl;
                         for (const auto &child : family.second) {
                             cout << child << endl;
@@ -40,7 +31,7 @@ int main() {
                         cout << endl;
                     }
                     break;
-            case 4: exit(0);
+            case 3: exit(0);
         }
     }
     return 0;
